3405e595cbb06e60799fc68c8604fd81694c2bd3
mgordienko <mgordienko@OEM41218.nic.etu>
2022-12-23 16:53:27 +0300

    Создал папки и main файлы

---
+++ blink_led/Core/Inc/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: 23 дек. 2022 г.
+ *      Author: mgordienko
+ */
+
+#ifndef CORE_INC_MAIN_H_
+#define CORE_INC_MAIN_H_
+
+
+
+#endif /* CORE_INC_MAIN_H_ */
---
+++ blink_led/Core/Src/main.c
@@ -0,0 +1,8 @@
+/*
+ * main.c
+ *
+ *  Created on: 23 дек. 2022 г.
+ *      Author: mgordienko
+ */
+
+
